69a12638761980eb4c45be2d2193cbd7849a39d1
appliraz <114522959+appliraz@users.noreply.github.com>
2023-10-26 23:29:26 +0300

    Update constants.js

--- src/AppLogic/constants.js
+++ src/AppLogic/constants.js
@@ -49,7 +49,7 @@ export const constants = {
 
 
 export const serverExtensions = {
-    server_url: "http://127.0.0.1:5000/",
+    server_url: "https://flask-musical-instruments-server.vercel.app/",
     download: "download",
     scrap: "scrap",
     supported_websites: "websites"
